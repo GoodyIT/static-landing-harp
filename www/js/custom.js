12bb04e99815bf9d8e6494cf6917cf1d70ff07bb
ideveloper003 <ideveloper003@gmail.com>
2019-09-13 13:15:50 -0400

    add moving card for service

--- www/js/custom.js
+++ www/js/custom.js
@@ -7,3 +7,33 @@
  * License URI:http://support.wrapbootstrap.com/
  * File Description: Place here your custom scripts
  */
+
+$(window).on("load", function() {
+    var $landingWrapper = $(".home-service-wrapper"),
+        $landingInnerContent = $(".home-service-list");
+
+    // set initial container to half of .landing-inner-content width
+    //TweenMax.set($landingWrapper, {scrollTo: {x: $landingInnerContent.width()/4}, ease: Power2.easeOut});
+
+    // scroll left and right
+    $landingInnerContent.on("mousemove touchmove", function(e) {
+        if (e.clientX > $landingWrapper.width() / 2) {
+        TweenMax.to($landingWrapper, 2, {
+            scrollTo: {
+            x: "+=175"
+            },
+            ease: Power2.easeOut
+        });
+        } else {
+        TweenMax.to($landingWrapper, 2, {
+            scrollTo: {
+            x: "-=175"
+            },
+            ease: Power2.easeOut
+        });
+        }
+    });
+
+    });
+
+  
